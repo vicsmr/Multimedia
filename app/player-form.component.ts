cff054b622d693983576d690f80c25b8456a29a2
Victor Sanchez <vicsmr94@gmail.com>
2016-04-17 19:35:55 +0200

    Funcionalidad para poder elegir el equipo del jugador

--- app/player-form.component.ts
+++ app/player-form.component.ts
@@ -1,6 +1,7 @@
-import {Component}  from 'angular2/core';
+import {Component, OnInit}  from 'angular2/core';
 import {RouteParams, Router} from 'angular2/router';
 import {Player, PlayerService}   from './player.service';
+import {Team, TeamService}   from './team.service';
 
 @Component({
   selector: "player-form",
@@ -11,11 +12,13 @@ export class PlayerFormComponent {
 
   newPlayer: boolean;
   player: Player;
+  teams: Team[];
 
   constructor(
     private router:Router,
     routeParams:RouteParams,
-    private service: PlayerService){
+    private service: PlayerService,
+    private teamservice: TeamService){
 
       let id = routeParams.get('id');
       if(id){
@@ -29,6 +32,13 @@ export class PlayerFormComponent {
         this.newPlayer = true;
       }
   }
+
+  ngOnInit(){
+      this.teamservice.getTeams().subscribe(
+        teams => this.teams = teams,
+        error => console.log(error)
+      );
+    }
 
   cancel() {
     window.history.back();
