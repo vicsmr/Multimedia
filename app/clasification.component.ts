67ef6b3e7d05faa2467d90744a4120451b39b7ee
flopezn11 <fernando_lopeznieto@hotmail.es>
2016-04-20 18:41:02 +0200

    Clasificación
    En clasificación ya salen solo los equipos creados, además he añadido
    una variable en el team.service de puntos para hacer la clasificación

--- app/clasification.component.ts
+++ app/clasification.component.ts
@@ -1,5 +1,6 @@
-import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {Component,OnInit} from 'angular2/core';
+import {ROUTER_DIRECTIVES,RouteParams, Router} from 'angular2/router';
+import {Team, TeamService}   from './team.service';
 
 @Component({
 	selector: 'clasification',
@@ -7,6 +8,17 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 	templateUrl: 'app/html/clasification.component.html',
 	styleUrls: ['app/css/clasification.component.css'],
 })
-export class ClasificationComponent {
+export class ClasificationComponent implements OnInit {
+	teams: Team[];
+
+  constructor(private router:Router, private service: TeamService) {}
+
+  ngOnInit(){
+      this.service.getTeams().subscribe(
+        teams => this.teams = teams,
+        error => console.log(error)
+      );
+    }
+
 
 }
