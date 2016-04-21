a2ba4f8e654d8fef7aa5a56caad3e3a49f326925
Victor Sanchez <vicsmr94@gmail.com>
2016-04-21 13:52:13 +0200

    Parte del formulario de los partidos

--- app/match-form.component.ts
+++ app/match-form.component.ts
@@ -1,7 +1,8 @@
-import {Component}  from 'angular2/core';
+import {Component, OnInit}  from 'angular2/core';
 import {RouteParams, Router} from 'angular2/router';
 import {Match, MatchService}   from './match.service';
 import {Schedule, ScheduleService}   from './schedule.service';
+import {Team, TeamService}   from './team.service';
 
 @Component({
   templateUrl: "app/html/match-form.component.html",
@@ -12,12 +13,18 @@ export class MatchFormComponent {
   newMatch: boolean;
   match: Match;
   schedule: Schedule;
+  teams: Team[];
+  idlocal: number;
+  idvisitor: number;
+  teamlocal: Team;
+  teamvisitor: Team;
 
   constructor(
     private _router:Router,
     routeParams:RouteParams,
     private service: MatchService,
     private scheduleService: ScheduleService,
+    private teamService: TeamService,
     ){
 
       let id = routeParams.get('id');
@@ -38,11 +45,28 @@ export class MatchFormComponent {
       }
   }
 
+  ngOnInit(){
+        this.teamService.getTeams().subscribe(
+        teams => this.teams = teams,
+        error => console.log(error)
+        );
+    }
+
   cancel() {
     window.history.back();
   }
 
   save() {
+    this.teamService.getTeam(this.idlocal).subscribe(
+    team => this.teamlocal = team,
+    error => console.log(error)
+    );
+    this.teamService.getTeam(this.idvisitor).subscribe(
+    team => this.teamvisitor = team,
+    error => console.log(error)
+    );
+    this.match.local = this.teamlocal;
+    this.match.visitor = this.teamvisitor;
     this.match.schedule = this.schedule;
     this.service.saveMatch(this.match);
     window.history.back();
--- app/match.service.ts
+++ app/match.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from 'angular2/core';
 import {Observable} from 'rxjs/Observable';
 import {withObserver} from './utils';
 import {Schedule} from './schedule.service';
+import {Team} from './team.service';
 
 export class Match {
 
@@ -9,6 +10,8 @@ export class Match {
     public id: number,
     public date: string,
     public schedule: Schedule,
+    public local: Team,
+    public visitor: Team,
     ) {}
 
 }
@@ -17,9 +20,10 @@ export class Match {
 export class MatchService {
 
   private matchs = [
-  	new Match(1, "21 Marzo", new Schedule(1)),
-  	new Match(2, "22 Marzo", new Schedule(2)),
-  	new Match(3, "23 Marzo", new Schedule(2)),
+  	new Match(1, "21 Marzo", new Schedule(1), new Team(1, 'New York City', 'http://futhead.cursecdn.com/static/img/15/clubs/689.png', 'https://elrincondejoost.files.wordpress.com/2014/04/red-bulls-ny.jpg', 'El New York City es un club de fútbol de Estados Unidos de la ciudad de New York, en Nueva York. Fue fundado en 1992 por la familia Saputo y juega actualmente en la Conferencia Este de la Major League Soccer.', 'Historia'),
+    new Team(3, 'Columbus Crew', 'http://www.moddingway.com/fifadb/teams/l687.png', 'https://s3.amazonaws.com/user-media.venngage.com/305423-48ce8432e479aa7c2e7789bf07c0f307.jpg', 'El Columbus Crew es un club de fútbol de Estados Unidos de la ciudad de Columbus, en Columbia. Fue fundado en 1992 por la familia Saputo y juega actualmente en la Conferencia Este de la Major League Soccer.', 'Historia')),
+  	// new Match(2, "22 Marzo", new Schedule(2)),
+  	// new Match(3, "23 Marzo", new Schedule(2)),
   ];
 
   getMatches() {
@@ -28,7 +32,7 @@ export class MatchService {
 
   getMatch(id: number | string) {
     let match = this.matchs.filter(h => h.id === +id)[0]
-    return withObserver(new Match(match.id, match.date, match.schedule));
+    return withObserver(new Match(match.id, match.date, match.schedule, match.local, match.visitor));
   }
 
   removeMatch(match: Match){
@@ -46,6 +50,8 @@ export class MatchService {
       let oldMatch = this.matchs.filter(h => h.id === match.id)[0];
       oldMatch.fecha = match.fecha;
       oldMatch.schedule = match.schedule;
+      oldMatch.local = match.local;
+      oldMatch.visitor = match.visitor;
     } else {
       match.id = this.matchs.length+1;
       this.matchs.push(match);
