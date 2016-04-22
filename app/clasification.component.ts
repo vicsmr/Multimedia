1d244afd7ff78e7166d05d6dc67105709b15dd43
Victor Sanchez <vicsmr94@gmail.com>
2016-04-22 16:45:42 +0200

    Elegir ultima jornada

--- app/clasification.component.ts
+++ app/clasification.component.ts
@@ -1,6 +1,9 @@
 import {Component,OnInit} from 'angular2/core';
-import {ROUTER_DIRECTIVES,RouteParams, Router} from 'angular2/router';
+import {ROUTER_DIRECTIVES, RouteParams, Router} from 'angular2/router';
 import {Team, TeamService}   from './team.service';
+import {Match, MatchService} from './match.service';
+import {Login, LoginService} from './login.service';
+import {Schedule, ScheduleService}   from './schedule.service';
 
 @Component({
 	selector: 'clasification',
@@ -10,14 +13,26 @@ import {Team, TeamService}   from './team.service';
 })
 export class ClasificationComponent implements OnInit {
 	teams: Team[];
+	matches: Match[];
+	schedules: Schedule[];
+	journey: number;
 
-  constructor(private router:Router, private service: TeamService) {}
+  constructor(private router:Router, private service: TeamService, private matchService: MatchService, private loginService: LoginService, private scheduleService: ScheduleService) {}
 
   ngOnInit(){
       this.service.getTeams().subscribe(
         teams => this.teams = teams,
         error => console.log(error)
       );
+			this.matchService.getMatches().subscribe(
+			matches => this.matches = matches,
+			error => console.log(error)
+			);
+			this.scheduleService.getSchedules().subscribe(
+			schedules => this.schedules = schedules,
+			error => console.log(error)
+			);
+			this.journey = 1;
     }
 
 
